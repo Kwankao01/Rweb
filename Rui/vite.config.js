45825bfcec8a8a647e2ceea6a36676901927a725
Nuchpravi <nuchpravi_pin64@tbs.tu.ac.th>
2024-11-18 14:10:33 +0700

    fast

--- Rui/vite.config.js
+++ Rui/vite.config.js
@@ -2,5 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy:{
+			//Proxy API request to FastAPI
+			'/api': {
+				target:'http://127.0.0.1:8000', //FastAPI server
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/,''), //Remove '/api' prefix when forwarding
+			},
+		},
+	},
 });
